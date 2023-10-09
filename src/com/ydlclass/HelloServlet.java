292da8b33556eecb9c1c02d2f6f9f1691833fcee
tangyubin <413593545@qq.com>
2023-10-09 16:29:59 +0800

    first start tomcat

---
+++ src/com/ydlclass/HelloServlet.java
@@ -0,0 +1,37 @@
+package com.ydlclass;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("init()被执行了一次");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("service()被执行了一次");
+        PrintWriter writer = servletResponse.getWriter();
+        writer.print("hello service");
+        writer.flush();
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy()被执行了一次");
+    }
+}
